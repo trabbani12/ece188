aa4e9843cb5ce308f6a9605b0c933a1885601e49
cmarshal <cmarshal@ucsd.edu>
2017-06-13 07:09:44 +0000

    added light commands

--- motion_detection/logic.py
+++ motion_detection/logic.py
@@ -1,8 +1,17 @@
 from roku import Roku
 from time import sleep
+from phue import Bridge
+import logging
+import random
+logging.basicConfig()
 
+b = Bridge('192.168.0.117')
 
+b.connect()
 
+b.get_api()
+
+lights = b.get_light_objects()
 
 def roku_command(remote_command):
     delay = 1
@@ -12,12 +21,16 @@ def roku_command(remote_command):
     roku = Roku('192.168.0.110')
 
     #remote command is place holder for list of commands from remote to dashboard
-    #remote_command=[]
+    #remote_command=['triangle']
 
     #if circle shape is registered on remote than launch the most recent episode of a users favorite show.
     #In this case Silicon Valley.
     for i in remote_command:
         if i == 'circle':
+
+            for light in lights:
+                light.brightness = 254
+                light.xy = [.1,.1]
 
             hbo = roku["HBO GO"]
             hbo.launch()
@@ -27,7 +40,7 @@ def roku_command(remote_command):
             roku.down()
             sleep(delay)
 
-            roku.down()
+            roku.right()
             sleep(delay)
 
             roku.right()
@@ -36,29 +49,37 @@ def roku_command(remote_command):
             roku.select()
             sleep(3*delay)
 
-            roku.up()
-            sleep(delay)
+	    roku.select()
+	    sleep(2*delay)
 
-            roku.left()
-            sleep(delay)
+	    roku.select()
+	    sleep(delay)	    
+#            roku.up()
+#            sleep(delay)
 
-            roku.down()
-            sleep(delay)
+#            roku.left()
+#            sleep(delay)
 
-            roku.left()
-            sleep(delay)
+#            roku.down()
+#            sleep(delay)
 
-            roku.select()
-            sleep(2*delay)
+#            roku.left()
+#            sleep(delay)
 
-            roku.right()
-            sleep(delay)
+#            roku.select()
+#            sleep(2*delay)
 
-            roku.select()
-            sleep(2*delay)
+#            roku.right()
+#            sleep(delay)
 
-            roku.select()
-            sleep(2*delay)
+#            roku.select()
+#            sleep(2*delay)
+
+#            roku.select()
+#            sleep(2*delay)
+
+            for light in lights:
+                light.brightness = 100
 
         if i == "left_swipe":
             #move cursor left
@@ -75,15 +96,33 @@ def roku_command(remote_command):
             roku.right()
 
         if i == "triangle":
+	    print "test"
+	    b.set_light([2,3], 'on', True)
+
+	    # Prints if light 1 is on or not
+	    b.get_light(2, 'on')
+
+	    # Set brightness of lamp 1 to max
+	    b.set_light(2, 'bri', 254)
+
+	    # Set brightness of lamp 2 to 50%
+	    b.set_light(3, 'bri', 254)
+
+	    for light in lights:
+		light.brightness = 254
+		light.xy = [.9,.7]	
             #enter search commands
             roku.search()
 
         if i == "square":
             roku.info()
 
+	if i == 'quad_diagonal_1':
+	    b.set_light(1, 'on', False)
+
     return True
 
-#variable = "right_swipe""
+#variable = "circle"
 #list=[]
 #list.append(variable)
 #roku_command(list)
